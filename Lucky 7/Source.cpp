71df0979e8f54704f49acdf4aae96535e9647317
Kyle Brandimarte <kylebrandimarte2@gmail.com>
2020-01-09 12:02:31 -0700

    First attempt at program 1

---
+++ Lucky 7/Source.cpp
@@ -0,0 +1,25 @@
+//Kyle Brandimarte
+//This is my own work
+//In class exercise 1/8/2020
+#include <iostream>
+using namespace std;
+int main() {
+ double lb, kg;
+	cout << "Weight in pounds: ";
+	cin >> lb;
+	kg = lb * 0.454;
+	cout << "Weight in kilograms: " << kg ;
+
+	double s, area;
+	cout << "length: ";
+	cin >> s;
+		area = [(3 *(3) ^ 1 / 2) / 2] * s ^ 2;
+	cout << "Answer: " << area >> ;
+	system("pause");
+
+
+
+
+
+	return 0;
+}
